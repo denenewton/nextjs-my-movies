73ba3c08ae5cd93a9c3fb47af2cb26bea6b5f1b6
Daniel Santos <denenewton@icloud.com>
2024-02-22 01:24:51 -0300

    add

--- components/GenreList.jsx
+++ components/GenreList.jsx
@@ -1,20 +1,23 @@
-'use client'
-import { getCollectionByGenre } from "../utils/firebase.utils";
-import { Button, HStack, List, ListItem,  Heading, useColorModeValue} from "@chakra-ui/react";
-import { useEffect } from "react";
+"use client";
+import { Button, HStack, List, ListItem, Heading } from "@chakra-ui/react";
 import useSearchContext from "../hook/useSearchContext";
-//import { useContext } from "react";
-//import QueryContext from "@/contexts/QueryContext";
 
+const GenreList = () => {
+  const genres = [
+    "Science_fiction",
+    "Adventure",
+    "Drama",
+    "Romance",
+    "Teen",
+    "Action",
+    "Thriller",
+    "Comedy",
+    "Fantasy",
+    "All Genres",
+  ];
+  const { searchText, setGenreMovies, searchGenre, setSearchGenre } =
+    useSearchContext();
 
-const GenreList = () =>  {
-  const genres = ['Science fiction','Adventure','Drama' ,'Romance' , 'Teen','Action', 'Thriller','Comedy', 'Fantasy', 'All Genres']
-  const {searchText, setGenreMovies  ,searchGenre,setSearchGenre,} = useSearchContext()
-  //const { selectGenreText, dispatchGenre } = useContext(QueryContext);
-  ///const color = useColorModeValue('gray.700','')
-
-
-  
   return (
     <>
       <Heading fontSize="2xl" marginTop={10} marginBottom={3}>
@@ -24,17 +27,15 @@ const GenreList = () =>  {
         {genres.map((genre, index) => (
           <ListItem key={index} paddingY="5px">
             <HStack>
-
               <Button
-                 fontWeight={genre === searchGenre ? "bold" : "normal"}
-                 onClick={() => setSearchGenre(genre)}
+                fontWeight={genre === searchGenre ? "bold" : "normal"}
+                onClick={() => setSearchGenre(genre)}
                 fontSize="lg"
                 variant="link"
                 //color={color}
               >
-                {genre !== 'Science_Fiction'?  genre : 'Science fiction'}
+                {genre !== "Science_Fiction" ? genre : "Science fiction"}
               </Button>
-
             </HStack>
           </ListItem>
         ))}
@@ -43,6 +44,4 @@ const GenreList = () =>  {
   );
 };
 
-
-export default GenreList
-
+export default GenreList;
